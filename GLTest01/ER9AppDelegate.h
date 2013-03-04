dde91872a68d3cf6762ede1477a28818b8f95171
Cesar Luis Valdez <cesarvr@MacBook-Pro-de-Cesar.local>
2013-03-04 18:59:08 +0100

    Initial Commit

---
+++ GLTest01/ER9AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ER9AppDelegate.h
+//  GLTest01
+//
+//  Created by Cesar Luis Valdez on 04/03/13.
+//  Copyright (c) 2013 Cesar Luis Valdez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ER9AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GLTest01/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GLTest01
+//
+//  Created by Cesar Luis Valdez on 04/03/13.
+//  Copyright (c) 2013 Cesar Luis Valdez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ER9AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ER9AppDelegate class]));
+    }
+}
